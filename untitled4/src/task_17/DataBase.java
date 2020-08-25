e829a961ad91dd9e8bae9e5cf6eb9e7d6d3ec71a
ArsenYeghikyan <arsegikyan@gmail.com>
2020-08-25 21:06:02 +0400

    17

---
+++ untitled4/src/task_17/DataBase.java
@@ -0,0 +1,20 @@
+package task_17;
+
+import java.util.Scanner;
+
+public class DataBase {
+
+public void printOldestParticipants(){
+    Scanner scanner = new Scanner(System.in);
+    String name = scanner.next();
+    int age = scanner.nextInt();
+    scanner.close();
+    Human[] humans = new Human[];
+
+
+
+}
+
+
+
+}
