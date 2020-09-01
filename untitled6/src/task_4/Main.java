49d2cf9eea60e9a908493fb06ed95a527da79e65
ArsenYeghikyan <arsegikyan@gmail.com>
2020-09-01 20:54:05 +0400

    homeWork_6

--- untitled6/src/task_4/Main.java
+++ untitled6/src/task_4/Main.java
@@ -1,11 +1,9 @@
 package task_4;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.List.*;
+
 
 public class Main {
-    List<Integer> list = new ArrayList<>();
+
     public static void main(String[] args) {
       int[] a= ArraysCompare.intsArrayMaker(3);
         int[] a1 =ArraysCompare.intsArrayMaker(2);
