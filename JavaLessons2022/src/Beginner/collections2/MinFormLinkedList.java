aff379c854ce9376a088997de2c10a42e8d85efd
ArsenYeghikyan <arsegikyan@gmail.com>
2022-04-20 12:52:00 +0400

    20.04.2022. new

--- JavaLessons2022/src/Beginner/collections2/MinFormLinkedList.java
+++ JavaLessons2022/src/Beginner/collections2/MinFormLinkedList.java
@@ -3,16 +3,16 @@ package Beginner.collections2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Comparator;
 import java.util.LinkedList;
 
 public class MinFormLinkedList {
     public static void main(String[] args) {
 
         try {
-            LinkedList<Integer> integers = getIntegerList();
+            System.out.println(getMinValue());
 
 
-            System.out.println(integers);
         } catch (IOException ioException) {
 
             System.out.println("Wrong number");
@@ -39,6 +39,16 @@ public class MinFormLinkedList {
 
 
         return integers;
+    }
+
+
+    public static int getMinValue() throws IOException {
+        LinkedList<Integer> integers = getIntegerList();
+
+        integers.sort(Comparator.comparingInt(o -> o));
+        int minValue = integers.get(integers.size()-1);
+        System.out.println(integers);
+        return minValue;
     }
 
 
